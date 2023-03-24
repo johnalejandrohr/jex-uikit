5ce78659b55540d3e4f2b2fda85127e4928a391d
JohnAlejandro <johnalejandrohr@gmail.com>
2023-03-24 16:47:01 -0500

    implements astro

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    build: {
+        // Example: Generate `page.html` instead of `page/index.html` during build.
+        format: 'file'
+      },
+      markdown: {
+        shikiConfig: {
+          theme: 'dark-plus'
+        }
+      },
+});
